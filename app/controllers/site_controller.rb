661b1862b79c407c3cc74f2f0e7639f893b44f94
Fernando Blat <ferblape@gmail.com>
2010-07-21 17:41:04 +0200

    Home carrousel

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,6 +1,7 @@
 class SiteController < ApplicationController
   
   def home
+    @pictures = Picture.find_random(5)
   end
   
   def about
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -16,13 +16,34 @@
 class Picture < ActiveRecord::Base
 
   belongs_to :species
-  
+
   def self.pictures_directory(family)
     "/images/data/#{family}"
   end
   
   def public_filename
-    self.class.pictures_directory(species.family) + '/' + filename
+    file_path = self.class.pictures_directory(species.family) + '/' + filename
+    if File.file?(file_path)
+      file_path
+    else
+      "/images/default_big.png"
+    end
   end
+  
+  def self.find_random(limit = 10)
+    limit = Picture.count < limit ? Picture.count : limit
+    result = []
+    tries = 0
+    while result.size < limit && tries < 3
+      picture_id = rand(limit) + 1
+      if picture = Picture.find_by_id(picture_id)
+        result << picture
+      else
+        tries += 1
+      end
+    end
+    result
+  end
+  
   
 end
