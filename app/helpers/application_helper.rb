6c9e6a13edea82752d6a3fc46dd30c0b5983a752
Fernando Blat <ferblape@gmail.com>
2010-08-10 18:27:49 +0200

    Share in twitter and FB links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,12 @@ module ApplicationHelper
     entry.species_id ? "single" : "amount"
   end
   
+  def share_in_facebook(species)
+    "javascript:var%20d=document,f='http://www.new.facebook.com/share',l=d.location,e=encodeURIComponent,p='.php?src=bm&v=4&i=1219930092&u=#{CGI.escape(species_url(species))}&t='+e(d.title);1;try{if%20(!/^(.*\.)?facebook\.[^.]*$/.test(l.host))throw(0);share_internal_bookmarklet(p)}catch(z)%20{a=function()%20{if%20(!window.open(f+'r'+p,'sharer','toolbar=0,status=0,resizable=1,width=626,height=436'))l.href=f+p};if%20(/Firefox/.test(navigator.userAgent))setTimeout(a,0);else{a()}}void(0)"
+  end
+  
+  def share_in_twitter(species)
+    "http://twitter.com/?status=#{CGI.escape("#{species.full_name} - #{species_url(species)}")}"
+  end
+  
 end
