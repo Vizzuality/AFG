cd10ff4f6abdbeb58cfcc7e0aba67b0a70ef72e0
Fernando Blat <ferblape@gmail.com>
2010-08-17 07:57:58 +0200

    Removing map by symbol

--- app/models/taxonomy.rb
+++ app/models/taxonomy.rb
@@ -1,49 +1,49 @@
 class Taxonomy
   
   def self.kingdoms
-    kingdoms = Species.select("distinct(kingdom)").map(&:kingdom).delete_if{ |k| k.nil? || k == 'Unknown' }
+    kingdoms = Species.select("distinct(kingdom)").map{ |x| x.kingdom}.delete_if{ |k| k.nil? || k == 'Unknown' }
     kingdoms.map do |k|
       [k, Species.count(:conditions => {:kingdom => k})]
     end
   end
   
   def self.phylums(kingdom)
-    phylums = Species.select("distinct(phylum)").where(:kingdom => kingdom).map(&:phylum).delete_if{ |k| k.nil? || k == 'Unknown' }
+    phylums = Species.select("distinct(phylum)").where(:kingdom => kingdom).map{|x| x.phylum}.delete_if{ |k| k.nil? || k == 'Unknown' }
     phylums.map do |p|
       [p, Species.count(:conditions => {:kingdom => kingdom, :phylum => p})]
     end
   end
   
   def self.t_classes(kingdom, phylum)
-    t_classes = Species.select("distinct(t_class)").where({:kingdom => kingdom, :phylum => phylum}).map(&:t_class).delete_if{ |k| k.nil? || k == 'Unknown' }
+    t_classes = Species.select("distinct(t_class)").where({:kingdom => kingdom, :phylum => phylum}).map{|x| x.t_class}.delete_if{ |k| k.nil? || k == 'Unknown' }
     t_classes.map do |tc|
       [tc, Species.count(:conditions => {:kingdom => kingdom, :phylum => phylum, :t_class => tc})]
     end
   end
   
   def self.t_orders(kingdom, phylum, t_class)
-    t_orders = Species.select("distinct(t_order)").where({:kingdom => kingdom, :phylum => phylum, :t_class => t_class}).map(&:t_order).delete_if{ |k| k.nil? || k == 'Unknown' }
+    t_orders = Species.select("distinct(t_order)").where({:kingdom => kingdom, :phylum => phylum, :t_class => t_class}).map{|x| x.to_order}.delete_if{ |k| k.nil? || k == 'Unknown' }
     t_orders.map do |to|
       [to, Species.count(:conditions => {:kingdom => kingdom, :phylum => phylum, :t_class => t_class, :t_order => to})]
     end
   end
 
   def self.families(kingdom, phylum, t_class, t_order)
-    families = Species.select("distinct(family)").where({:kingdom => kingdom, :phylum => phylum, :t_class => t_class, :t_order => t_order}).map(&:family).delete_if{ |k| k.nil? || k == 'Unknown' }
+    families = Species.select("distinct(family)").where({:kingdom => kingdom, :phylum => phylum, :t_class => t_class, :t_order => t_order}).map{|x| x.family}.delete_if{ |k| k.nil? || k == 'Unknown' }
     families.map do |f|
       [f, Species.count(:conditions => {:kingdom => kingdom, :phylum => phylum, :t_class => t_class, :t_order => t_order, :family => f})]
     end
   end
 
   def self.genus(kingdom, phylum, t_class, t_order, family)
-    genus = Species.select("distinct(genus)").where({:kingdom => kingdom, :phylum => phylum, :t_class => t_class, :t_order => t_order, :family => family}).map(&:genus).delete_if{ |k| k.nil? || k == 'Unknown' }
+    genus = Species.select("distinct(genus)").where({:kingdom => kingdom, :phylum => phylum, :t_class => t_class, :t_order => t_order, :family => family}).map{|x| x.genus}.delete_if{ |k| k.nil? || k == 'Unknown' }
     genus.map do |g|
       [g, Species.count(:conditions => {:kingdom => kingdom, :phylum => phylum, :t_class => t_class, :t_order => t_order, :family => family, :genus => g})]
     end
   end
 
   def self.species(kingdom, phylum, t_class, t_order, family, genus)
-    species = Species.select("distinct(name)").where({:kingdom => kingdom, :phylum => phylum, :t_class => t_class, :t_order => t_order, :family => family, :genus => genus}).map(&:name).delete_if{ |k| k.nil? || k == 'Unknown' }
+    species = Species.select("distinct(name)").where({:kingdom => kingdom, :phylum => phylum, :t_class => t_class, :t_order => t_order, :family => family, :genus => genus}).map{|x| x.name}.delete_if{ |k| k.nil? || k == 'Unknown' }
     species.map do |s|
       [s, Species.count(:conditions => {:kingdom => kingdom, :phylum => phylum, :t_class => t_class, :t_order => t_order, :family => family, :genus => genus, :name => s})]
     end
