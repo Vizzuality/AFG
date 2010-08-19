b6171977448bf12eeacabce09ca719bde84b2df3
Javier Álvarez Medina <jmedina@Javier-Alvarez-Medinas-MacBook-Pro.local>
2010-08-19 11:50:27 +0200

    logos changes, Antarctic name replaces, html changes, some more bugs, etc

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   
   def title
-    page_title = ["Atlantic Field Guides"]
+    page_title = ["Antarctic Field Guides"]
     page_title << "Guides" if controller_name == 'guides'
     page_title << @guide.name if @guide
     page_title.join(' - ')
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,5 @@
+var modal_publish;
+
 AFG = {
 	behaviour: function() {
 		// AJAX pagination
@@ -6,15 +8,26 @@ AFG = {
 }
 
 $(document).ready(function() {
+	
+	modal_publish = $('#publish_container').html();
 
 	$('#search_term').example('Search species, guides,...');
 	$('#pop_up').delay(3000).fadeOut();
 	
 	// dragg elements >> ALL PAGES
-	$(function() {
-		$("#sortable").sortable({items: 'li:not(.dragg_here)',placeholder: 'place_draggable',tolerance: 'pointer'});
-		$("#sortable").disableSelection();
-	});
+	// $(function() {
+	// 	$("#sortable").sortable({items: 'li:not(.dragg_here)',placeholder: 'place_draggable',tolerance: 'pointer'});
+	// 	$("#sortable").disableSelection();
+	// });
+	
+	// Text shadow for IE >> MODAL
+	// $('#publish_container li.active span').textShadow({
+	// 	color:   "#000",
+	// 	xoffset: "5px",
+	// 	yoffset: "5px",
+	// 	radius:  "5px",
+	// 	opacity: "50"
+	// });
 	
 	//hover species list effect >> HOME
 	$('div.left ul li div.specie').hover(function(ev){
@@ -58,8 +71,7 @@ $(document).ready(function() {
 		$(this).parent().fadeOut();
 	});
 	
-	
-       
+
 	//hover created list effect >> EXPLORE
 	$('div.most_popular_explore div.content ul li').hover(function(ev){
 		$(this).css('background-position','0px -177px');
@@ -78,6 +90,8 @@ $(window).load(function() {
 });
 
 
+
+//Lastest Activity function >> HOME
 function changeList() {
 	$('div.right div.latest_ ul li:first div').removeClass('first');
 	$('div.right div.latest_ ul li:eq(4) div').animate({opacity: 0}, 700);
@@ -94,7 +108,103 @@ function changeList() {
 	});
 }
 
+
+
+//Pop up close action
 function closePopUp() {
 	$('#pop_up').fadeOut();
 }
+
+
+
+//Modal publish window
+function openPublish() {
+	$('#publish_container').html(modal_publish);
+	$('#publish_container').modal(
+		{onOpen: function (dialog) {
+						 	dialog.overlay.fadeIn('slow', function () {
+								dialog.data.hide();
+								dialog.container.fadeIn('slow', function () {
+									dialog.data.fadeIn('slow');
+								});
+							});
+						 },
+		onClose: 	function (dialog) {
+								dialog.data.fadeOut('fast', function () {
+									dialog.container.hide('fast', function () {
+										dialog.overlay.fadeOut('fast', function () {
+											$.modal.close();
+										});
+									});
+								});
+							}
+	});
+}
+
+
+
+function firstStep(type) {
+	
+	$('div#publish_container ul li').each(function(index,element){
+		if (index==1) {
+			$(this).addClass('active');
+		} else {
+			$(this).removeClass('active');
+		}
+	});
+	
+	
+	$('div.choice').animate({
+	    height: 'toggle'
+	  }, 500, function() {
+			var second_step = '<div class="long"><div class="type loading"><img class="loading" src="../images/modal/ajax-loader.gif" /></div><div class="info"><h5>Your Anctartic Field Guide is ready to download</h5><p>Please, be patient, this will take less than five minutes.</p><a class="disabled" href="#">Download</a></div></div>';
+	    $('div.choice').html(second_step);
+			$('div.choice').animate({
+			    height: 'toggle'
+			  }, 500, function() {
+					$('div.choice').delay(4000).animate({
+					    height: 'toggle'
+					  }, 500, function() {
+							var third_step = '<div class="long"><div class="type finished"><img class="image" src="" /><img class="ok" src="../images/modal/ok.png" /></div><div class="info"><h5>Your Anctartic Field Guide is ready to download</h5><p>If your download doesn’t start in five seconds, click the download button</p><a href="#">Download</a></div></div>';
+							$('div.choice').html(third_step);
+							$('div.choice').animate({
+							    height: 'toggle'
+							  }, 500);
+					});
+			});
+	  });
+}
+
+
+
+function secondStep(type) {
+	
+	$('div#publish_container ul li').each(function(index,element){
+		if (index==2) {
+			$(this).addClass('active');
+		} else {
+			$(this).removeClass('active');
+		}	});
+	
+	$('div.choice').animate({
+	    height: 'toggle'
+	  }, 500, function() {
+			var second_step = '<div class="long"><div class="type loading"><img class="loading" src="../images/modal/ajax-loader.gif" /></div><div class="info"><h5>Your Anctartic Field Guide is ready to download</h5><p>Please, be patient, this will take less than five minutes.</p><a class="disabled" href="#">Download</a></div></div>';
+	    $('div.choice').html(second_step);
+			$('div.choice').animate({
+			    height: 'toggle'
+			  }, 500, function() {
+					$('div.choice').delay(4000).animate({
+					    height: 'toggle'
+					  }, 500, function() {
+							var third_step = '<div class="long"><div class="type finished"><img class="image" src="" /><img class="ok" src="../images/modal/ok.png" /></div><div class="info"><h5>Your Anctartic Field Guide is ready to download</h5><p>If your download doesn’t start in five seconds, click the download button</p><a href="#">Download</a></div></div>';
+							$('div.choice').html(third_step);
+							$('div.choice').animate({
+							    height: 'toggle'
+							  }, 500);
+					});
+			});
+	  });
+}
+
 
