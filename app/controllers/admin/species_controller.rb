a698c33e1524eee328d4d9e2ffe40a967b9197b3
Fernando Blat <ferblape@gmail.com>
2010-08-30 18:38:39 +0200

    Fixing bug on deleting species

--- app/controllers/admin/species_controller.rb
+++ app/controllers/admin/species_controller.rb
@@ -82,7 +82,7 @@ class Admin::SpeciesController < ApplicationController
     @species.destroy
 
     respond_to do |format|
-      format.html { redirect_to(admin_species_url) }
+      format.html { redirect_to(admin_species_index_path, :notice => 'Species removed successfully') }
     end
   end
   
