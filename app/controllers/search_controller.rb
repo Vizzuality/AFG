a33ff41e603608de57c849ad9ceb4ad80b822123
Javier de la Torre <jatorre@vizzuality.com>
2010-08-31 16:56:16 +0200

    removed the need for a search param in the search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,29 +1,31 @@
 class SearchController < ApplicationController
   
   def index
+    results = []
     if params[:q].blank?
-      @results = []
+      @total_guides     = Guide.published.order("downloads_count DESC")
+      @total_species    = Species.order("guides_count DESC")
+      @total_landscapes = Landscape.order("guides_count DESC")
     else
-      results = []
       @total_guides     = Guide.find_by_term(params[:q])
       @total_species    = Species.complete.find_by_term(params[:q])
       @total_landscapes = Landscape.find_by_term(params[:q])
-      if params[:type]
-        results = case params[:type]
-          when 'species'
-            @total_species
-          when 'guides'
-            @total_guides
-          when 'landscapes'
-            @total_landscapes
-        end
-      else
-        results = (@total_guides + @total_species + @total_landscapes).sort{ |b,a| a.send(a.sort_by_attribute) <=> b.send(b.sort_by_attribute)}
+    end  
+    if params[:type]
+      results = case params[:type]
+        when 'species'
+          @total_species
+        when 'guides'
+          @total_guides
+        when 'landscapes'
+          @total_landscapes
       end
-      current_page = params[:page].blank? ? 1 : params[:page].to_i
-      @results = WillPaginate::Collection.create(current_page, 8, results.size) do |pager| 
-        pager.replace(results.slice(pager.per_page * (pager.current_page-1), pager.per_page))
-      end
+    else
+      results = (@total_guides + @total_species + @total_landscapes).sort{ |b,a| a.send(a.sort_by_attribute) <=> b.send(b.sort_by_attribute)}
+    end
+    current_page = params[:page].blank? ? 1 : params[:page].to_i
+    @results = WillPaginate::Collection.create(current_page, 8, results.size) do |pager| 
+      pager.replace(results.slice(pager.per_page * (pager.current_page-1), pager.per_page))
     end
   end
 
