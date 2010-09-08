35f9c9585b6a96d158bfdca7447ab9c36c70f8d1
Javier de la Torre <jatorre@vizzuality.com>
2010-09-08 13:20:18 +0200

    new sorting on the home page

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -4,7 +4,7 @@ class SiteController < ApplicationController
     @pictures = if Species.highlighted.count > 0 
       Species.complete.highlighted.limit(5).map{|s| s.picture}.compact.flatten
     end
-    @species = Species.complete.limit(6).order("featured, guides_count DESC")
+    @species = Species.complete.limit(6).order("featured DESC, guides_count DESC")
     @guides = Guide.highlighted.limit(3)
     @activities = Entry.limit(10).order('created_at DESC').delete_if{ |e| e.element.nil? }
   end
