c95e963a4785b57f403c9002b1e14c2d5cf645f8
Fernando Blat <ferblape@gmail.com>
2010-10-13 12:59:48 +0200

    Exceptions in staging should be named as staging

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,5 +1,7 @@
 HoptoadNotifier.configure do |config|
   config.api_key = '364cf4b6bf3b75ef9fa533c1ec0a24b4'
   config.js_notifier = true
-  config.development_environments = File.file?(Rails.root+'STAGING') ? %w(development test cucumber production) : %w(development test cucumber)
+  # Hide staging errors or not
+  # config.development_environments = File.file?(Rails.root+'STAGING') ? %w(development test cucumber production) : %w(development test cucumber)
+  config.environment_name = File.file?(Rails.root+'STAGING') ? 'staging' : 'production'
 end
