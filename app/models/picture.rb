fbe3500890f7b9b331009edb0da442f249c7c7e8
Fernando Blat <ferblape@gmail.com>
2010-11-08 10:46:15 +0100

    New sidebar image

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,24 +3,24 @@
 # Table name: pictures
 #
 #  id                 :integer         not null, primary key
-#  species_id         :integer         
-#  filename           :string(255)     
-#  title              :string(255)     
-#  caption            :string(255)     
-#  photographer       :string(255)     
-#  locality           :string(255)     
-#  created_at         :datetime        
-#  updated_at         :datetime        
-#  image_file_name    :string(255)     
-#  image_content_type :string(255)     
-#  image_file_size    :integer         
-#  image_updated_at   :datetime        
-#  description        :text            
+#  species_id         :integer
+#  filename           :string(255)
+#  title              :string(255)
+#  caption            :string(255)
+#  photographer       :string(255)
+#  locality           :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  image_file_name    :string(255)
+#  image_content_type :string(255)
+#  image_file_size    :integer
+#  image_updated_at   :datetime
+#  description        :text
 #
 
 class Picture < ActiveRecord::Base
 
   belongs_to :species
-  
-  has_attached_file :image, :styles => { :small => "48x48#", :medium => "168x110#", :large => "247x158#", :huge => "896x381#", :gallery => "896x896>"}
+
+  has_attached_file :image, :styles => { :small => "48x48#", :sidebar => "76x76#", :medium => "168x110#", :large => "247x158#", :huge => "896x381#", :gallery => "896x896>"}
 end
